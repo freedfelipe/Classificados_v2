fbfe255921c9187779010b9cdb6e2c1de1966b8e
Felipe Albert <freedfelipe@gmail.com>
2013-11-05 19:21:57 -0200

    Modulo de categorias de publicidade alterado

--- application/models/advertising_category_model.php
+++ application/models/advertising_category_model.php
@@ -22,7 +22,7 @@ class Advertising_category_model extends CI_Model{
 		$query = $this->db->get_where($this->tablename, $by);
 		
 		if($query->num_rows() > 0){
-			return $query->result_array();
+			return $query->row_array();
 		}
 		
 		return false;
@@ -44,64 +44,6 @@ class Advertising_category_model extends CI_Model{
 		return false;
 	}
 	
-	public final function total($start=0)
-	{
-		$this->db->where(array('status_id' => 1));
-		
-		return $this->db->count_all_results($this->tablename);
-	}
-	
-	public final function read_pag($limit = 0, $page_now = 0, $search = null)
-	{
-		$result = array(
-                'count' => 0,
-                'rows' => array()
-            );
-		 
-		$this->db->select('*');
-		$this->db->from($this->tablename);
-		//$this->db->where(array('status_id' => 1));
-		if($search){ $this->db->like(array('name' => $search['seeking'])); }
-		$this->db->order_by('name');
-		
-		if(isset($limit))
-		{
-			$this->db->limit($limit, $page_now);
-		}
-		
-		$query = $this->db->get();
-		
-		if($query->num_rows() > 0){			
-			$result['rows'] = $query->result_array();
-			$result['count'] = $query->num_rows();
-			return $result;
-		}
-		
-		return false;
-	}
-	
-	public final function read($start=0)
-	{
-		$query = $this->db->get_where($this->tablename, array('status_id' => 1), $this->per_page, $start);
-		
-		if($query->num_rows() > 0){
-			return array($query->result(), $query->num_rows());
-		}
-		
-		return false;
-	}
-	
-	public final function all()
-	{
-		$query = $this->db->get_where($this->tablename, array('status_id' => 1));
-		
-		if($query->num_rows() > 0){
-			return $query->result();
-		}
-		
-		return false;
-	}
-	
 	public final function update($id = '', $idHash = '', $data = array())
 	{
 		$data['name'] 			= $this->input->post('name', TRUE);
@@ -114,6 +56,17 @@ class Advertising_category_model extends CI_Model{
 		
 		if($this->db->update($this->tablename, $data)){
 			return true;
+		}
+		
+		return false;
+	}
+	
+	public final function all($where = array())
+	{
+		$query = $this->db->get_where($this->tablename, $where);
+		
+		if($query->num_rows() > 0){
+			return $query->result_array();
 		}
 		
 		return false;
--- application/views/backend/advertising_categories/_form.php
+++ application/views/backend/advertising_categories/_form.php
@@ -1,36 +1,52 @@
-<form class="edt-form" method="post" accept-charset="utf-8">
-	<fieldset>
-		<legend>Dados da Marca</legend>
-		
-		<div class="field_row <?php if(form_error('height')){print('validation_error');}?>">
-			<div class="field_label">Nome:</div>
-			<div><input type="text" name="name" id="name" class="form_input" <?php if($this->router->method == 'update'){print(" value='".@$row[0]['name']."'");}else{?> value = "<?=set_value('name');?>"<? } ?>/></div>
+<? if(isset($error)){?>
+<div class="row">
+	<div class="col-md-4 col-sm-4">
+		<div class="alert alert-block alert-error ruby-red-bg">
+			<button data-dismiss="alert" class="close" type="button">×</button>
+			<h3 class="text-white">Atenção!</h3>
+			<?=$error;?>
 		</div>
-		
-		<div class="field_row <?php if(form_error('height')){print('validation_error');}?>">
-			<div class="field_label">Altura (height):</div>
-			<div><input type="text" name="height" id="height" class="form_input onlynumbers" <?php if($this->router->method == 'update'){print(" value='".@$row[0]['height']."'");}else{?> value = "<?=set_value('height');?>"<? } ?>/></div>
-		</div>
-		
-		<div class="field_row <?php if(form_error('width')){print('validation_error');}?>">
-			<div class="field_label">Largura (width):</div>
-			<div><input type="text" name="width" id="width" class="form_input onlynumbers" <?php if($this->router->method == 'update'){print(" value='".@$row[0]['width']."'");}else{?> value = "<?=set_value('width');?>"<? } ?>/></div>
-		</div>
-		
-		<div class="field_row <?php if(form_error('status_id')){print('validation_error');}?>">
-			<div class="field_label">Status:</div>
-			<div>
-				<select name="status_id" id="status_id" class="form_input">
-					<option value="-1">Selecione...</option>
-					<? if($this->router->method == 'update'){ $data_select =  $row[0]['status_id']; }else{ $data_select = set_value('status_id'); } status_select($data_select);?>
-				</select>
-			</div>
-		</div>
-	</fieldset>
-	
-	<br />
-	
-	<input type="submit" class="button" value="<?=$this->lang->line('button_save');?>" />
-	<input type="reset" class="button" value="<?=$this->lang->line('button_clear');?>" />
-	<input type="button" class="button" value="<?=$this->lang->line('button_back');?>" onclick="javascript:history.back();" />
-</form>
+	</div>
+</div>
+<? } ?>
+<div class="row">
+    <div class="col-md-6">
+        <div class="widget">
+            <div class="widget-body">
+                <form class="form-horizontal" role="form" method="post" action="<?=site_url($_SERVER['REQUEST_URI']);?>" accept-charset="utf-8">
+                    <div class="form-group <?php if(form_error('name')){echo 'has-error';}?>">
+                        <label class="col-lg-2 control-label">Nome</label>
+                        <div class="col-lg-10">
+                            <input type="Text" class="form-control" placeholder="Nome" name="name" value="<?=set_value('name', @$row['name'], $this->input->post('name')); ?>">
+                        </div>
+                    </div>
+					<div class="form-group <?php if(form_error('height')){echo 'has-error';}?>">
+                        <label class="col-lg-2 control-label">Altura (height)</label>
+                        <div class="col-lg-10">
+                            <input type="Text" class="form-control" placeholder="Altura" name="height" value="<?=set_value('height', @$row['height'], $this->input->post('height')); ?>">
+                        </div>
+                    </div>
+					<div class="form-group <?php if(form_error('width')){echo 'has-error';}?>">
+                        <label class="col-lg-2 control-label">Largura (width)</label>
+                        <div class="col-lg-10">
+                            <input type="Text" class="form-control" placeholder="Largura" name="width" value="<?=set_value('width', @$row['width'], $this->input->post('width')); ?>">
+                        </div>
+                    </div>
+					<div class="form-group <?php if(form_error('status_id')){echo 'has-error';}?>">
+                        <label class="col-lg-2 control-label">Status</label>
+                        <div class="col-lg-10">
+							<?php echo form_dropdown('status_id', construct_select(status()), set_value('status_id', @$row['status_id'], $this->input->post('status_id')), 'class="form-control"'); ?>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-lg-offset-2 col-lg-10">
+                            <button type="submit" class="btn btn-success">Salvar</button>
+							<button type="reset" class="btn btn-warning">Limpar</button>
+							<button type="button" class="btn btn-default" onclick="javascript:history.back();" >Voltar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
